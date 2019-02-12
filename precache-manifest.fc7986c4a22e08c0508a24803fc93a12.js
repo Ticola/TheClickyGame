295f39fc6555a4fee37e951508f413e27aea235a
Edgar Reynaga <edgar.reynaga@loop.colum.edu>
2019-02-12 01:41:05 -0600

    Updates

---
+++ precache-manifest.fc7986c4a22e08c0508a24803fc93a12.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "7dfb4c579b18c79c600b",
+    "url": "/TheClickyGame/static/css/main.450be7a4.chunk.css"
+  },
+  {
+    "revision": "7dfb4c579b18c79c600b",
+    "url": "/TheClickyGame/static/js/main.7dfb4c57.chunk.js"
+  },
+  {
+    "revision": "39a35ad48cad73baaa8f",
+    "url": "/TheClickyGame/static/js/runtime~main.39a35ad4.js"
+  },
+  {
+    "revision": "dcb82aee628a7a110fea",
+    "url": "/TheClickyGame/static/js/2.dcb82aee.chunk.js"
+  },
+  {
+    "revision": "9c46d7ee1055e4526ce8653d2d1cc19e",
+    "url": "/TheClickyGame/static/media/ScreenShot.9c46d7ee.png"
+  },
+  {
+    "revision": "78c2573bf81e7231fcf09f2975af7bbb",
+    "url": "/TheClickyGame/index.html"
+  }
+];
